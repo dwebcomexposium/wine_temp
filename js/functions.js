c8edbe1b1a4650f1b5db94b27471a31bf12a92b7
d-willb <39524812+d-willb@users.noreply.github.com>
2018-06-25 13:42:34 +0200

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,54 @@
+;(function($, window, document, undefined) {
+	var $win = $(window);
+	var $doc = $(document);
+	var scrolledY = 0;
+
+	//Animate Sections
+	var $animated = $('.animated');
+
+	var winHeight = $win.height();
+	var winWidth = $win.width();
+	var winScroll = $win.scrollTop();
+
+	function animateElements() {
+		winScroll = $win.scrollTop();
+
+		if ( $animated.length === 0 ) {
+			return;
+		}			
+	};
+	
+	function animate(winST) {
+		$('.animate').each(function(){
+			var $this = $(this);
+
+			if (winST + ($win.outerHeight() / 1.3) > $this.offset().top) {
+				$this.addClass('animated');
+			} else if (winST + ($win.outerHeight() / 2) < $this.offset().top) {
+				$this.removeClass('animated');
+			}		
+		});
+	}
+
+	$doc.ready(function() {
+		$('.btn-down').on('click', function(e) {
+			e.preventDefault();
+
+			var targetOT = $( $(this).parent().next() ).offset().top
+
+			$('html, body').animate({
+				scrollTop: targetOT
+			})
+		})
+	});
+
+	$win.on('load', function() {
+		animateElements();
+		animate(scrolledY);
+	}).on('scroll', function() {
+		scrolledY = $win.scrollTop();
+		animate(scrolledY);
+	})
+
+	
+})(jQuery, window, document);
